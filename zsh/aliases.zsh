a79b76e36b00067f8fa85399ae9878a7803136fa
Jon Erickson <jon@ericksn.com>
2019-03-08 11:48:48 -0800

    move rebuild-completions to alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #
 
+alias awscreds='cat $HOME/.aws/credentials'
 alias cls='clear'
 alias df='e $DOTFILES'
+alias rebuild-completions='rm -f ~/.zcompdump; compinit'
 alias show-ports='sudo lsof -iTCP -sTCP:LISTEN -n -P'
 alias uuid="uuidgen | tr -d '\n' | tr '[:upper:]' '[:lower:]' | pbcopy && pbpaste && echo"
-alias awscreds='cat $HOME/.aws/credentials'
